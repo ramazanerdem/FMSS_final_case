4bb03e1fbe6cc910b6cb75d0058418b9f8381ced
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-04-28 00:20:16 +0300

    mobile border removed

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 function AppLayout() {
   return (
-    <div className="border-x-2 border-white border-opacity-20 px-8 min-h-screen mx-auto sm:w-1/2">
+    <div className="border-x-0 sm:border-x-2 border-white border-opacity-20 px-8 min-h-screen mx-auto sm:w-1/2">
       <Header />
       <hr className="relative border-1 border-white border-opacity-30 mb-6 sm:mb-10" />
       <Outlet />
